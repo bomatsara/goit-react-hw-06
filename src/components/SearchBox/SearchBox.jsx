16a53d2f345d3949df5b62577b01273456396260
Andrii Bomatsara <andrii.bomatsara@vista.com>
2024-07-19 11:41:30 +0300

    Fix problem

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,7 @@
 import { useId } from 'react';
 import css from './SearchBox.module.css';
-import { useDispatch } from 'react-redux';
-import { changeFilter } from '../../redux/filtersSlice.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter, selectNameFilter } from '../../redux/filtersSlice.js';
 
 export default function SearchBox() {
   const fieldIds = {
@@ -9,6 +9,7 @@ export default function SearchBox() {
   };
 
   const dispatch = useDispatch();
+  const filterValue = useSelector(selectNameFilter);
 
   return (
     <div className={css['search-box']}>
@@ -16,8 +17,10 @@ export default function SearchBox() {
         <label htmlFor={fieldIds.search} className={css['form-label']}>Find contacts by name</label>
         <input
           className={css['form-input']}
-          type="text" name="search"
+          type="text"
+          name="search"
           id={fieldIds.search}
+          value={filterValue}
           onChange={(e) => dispatch(changeFilter(e.target.value))}
         />
       </div>
